c373caaecae06d018ccd9781c8d9d3d43143d924
Bryan Hadinata <bryanhadinata76@gmail.com>
2023-12-15 16:39:28 +0700

    chore: setting up layout and trying pages directory!

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,37 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Inter } from 'next/font/google'
+import Head from 'next/head'
+
+const inter = Inter({ subsets: ['latin'] })
+
+//this is like the layout.tsx in app directory!
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <title>Bryan Hadinata | Portfolio</title>
+                <meta
+                    name="description"
+                    content="Bryan Hadinata's Portfolio - Full Stack Web Developer"
+                />
+                <meta
+                    name="keywords"
+                    content="web development, web design, portfolio, Bryan Hadinata"
+                />
+                <meta name="author" content="Bryan Hadinata" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className={inter.className}>
+                <main>
+                    <Component {...pageProps} />
+                </main>
+            </div>
+        </>
+    )
 }
---
+++ src/pages/articles.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function ArticlesPage() {
+    return (
+        <>
+            <Head>
+                <title>Articles | Portfolio</title>
+                <meta
+                    name="description"
+                    content="Read the latest articles on Bryan Hadinata's portfolio page"
+                />
+            </Head>
+            <div>Hello, this is blog page!</div>
+        </>
+    )
+}
